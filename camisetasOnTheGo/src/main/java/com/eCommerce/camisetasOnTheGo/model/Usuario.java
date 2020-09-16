05fd563a40a8c8e1807f714c0e47b8dcf37bd43e
Thiago Crispim <thiagjack@gmail.com>
2020-09-16 12:17:35 -0300

    Tarefa 7

--- camisetasOnTheGo/src/main/java/com/eCommerce/camisetasOnTheGo/model/Usuario.java
+++ camisetasOnTheGo/src/main/java/com/eCommerce/camisetasOnTheGo/model/Usuario.java
@@ -9,12 +9,12 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
-@Table(name = "cliente")
-public class Cliente {
+@Table(name = "usuario")
+public class Usuario {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id_cliente;
+	private long id;
 	
 	@NotNull
 	@Size(min = 3, max = 100)
@@ -25,17 +25,15 @@ public class Cliente {
 	private String email;
 	
 	@NotNull
-	@Size (min = 5, max = 20)
+	@Size (min = 5, max = 100)
 	private String senha;
-	
-	
-	
-	public long getId_cliente() {
-		return id_cliente;
+
+	public long getId() {
+		return id;
 	}
 
-	public void setId_cliente(long id_cliente) {
-		this.id_cliente = id_cliente;
+	public void setId(long id) {
+		this.id = id;
 	}
 
 	public String getNome() {
@@ -61,8 +59,6 @@ public class Cliente {
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
-
-
+		
 	
-
 }
